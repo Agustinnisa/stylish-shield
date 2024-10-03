e3f88b49d884fecd22b8efe71a4b5af5c2dbd726
Agustinnisa <nisaagustin6@gmail.com>
2024-10-04 06:48:06 +0700

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import logo from '../assets/barterstyle-logo.svg';
 
 const Header = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand as={Link} to="/">
-        Weather Outfit Recommender
+    <Navbar bg="light" expand="lg" className="shadow-sm p-3 mb-5 bg-white rounded">
+      <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+        <img
+          src={logo}
+          width="60"
+          height="50"
+          className="d-inline-block align-top"
+          alt="BarterStyle logo"
+        />
+        <span className="ml-2" style={{ fontFamily: 'Pacifico, cursive', fontSize: '1.5rem', color: '#277f91' }}>
+        </span>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/" style={{ color: '#343a40', fontWeight: '500' }}>
             Dashboard
           </Nav.Link>
-          <Nav.Link as={Link} to="/detail">
+          <Nav.Link as={Link} to="/detail" style={{ color: '#343a40', fontWeight: '500' }}>
             Details
           </Nav.Link>
-          <Nav.Link as={Link} to="/weekly">
+          <Nav.Link as={Link} to="/weekly" style={{ color: '#343a40', fontWeight: '500' }}>
             Weekly Forecast
           </Nav.Link>
-          <Nav.Link as={Link} to="/outfit">
+          <Nav.Link as={Link} to="/outfit" style={{ color: '#343a40', fontWeight: '500' }}>
             Outfit Recommendations
           </Nav.Link>
         </Nav>
